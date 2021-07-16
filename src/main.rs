fedf3bc837ac18db099e6c5048e47d8d6c314bf0
John Salis <jsalis531@gmail.com>
2021-07-16 03:25:42 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,144 @@
+use macroquad::prelude::*;
+
+const BOARD_WIDTH: usize = 32;
+const BOARD_HEIGHT: usize = 54;
+const INITIAL_LAYER_WIDTH: u8 = 12;
+
+enum Direction {
+    LEFT,
+    RIGHT,
+}
+
+#[derive(Debug)]
+struct Game {
+    board: [[u8; BOARD_WIDTH]; BOARD_HEIGHT],
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {
+            board: [[0; BOARD_WIDTH]; BOARD_HEIGHT],
+        }
+    }
+}
+
+fn get_layer_width(board: [[u8; BOARD_WIDTH]; BOARD_HEIGHT], level: u8) -> u8 {
+    let mut count: u8 = 0;
+    for el in board[level as usize].iter() {
+        count += el;
+    }
+    count
+}
+
+#[macroquad::main("Stackem")]
+async fn main() {
+    let mut game = Game::new();
+    let mut move_direction = Direction::RIGHT;
+    let mut last_update = get_time();
+    let mut speed = 0.1;
+    let mut level = 0;
+    let mut game_over = false;
+    for i in 0..INITIAL_LAYER_WIDTH {
+        game.board[level as usize][i as usize] = 1;
+    }
+
+    loop {
+        clear_background(BLACK);
+
+        if game_over && is_key_pressed(KeyCode::Space) {
+            game_over = false;
+            speed = 0.1;
+            level = 0;
+            for i in 0..BOARD_HEIGHT {
+                for j in 0..BOARD_WIDTH {
+                    game.board[i][j] = 0;
+                }
+            }
+            for i in 0..INITIAL_LAYER_WIDTH {
+                game.board[level as usize][i as usize] = 1;
+            }
+        }
+
+        if !game_over && is_key_pressed(KeyCode::Down) {
+            if level > 0 {
+                for i in 0..BOARD_WIDTH - 1 {
+                    if game.board[(level - 1) as usize][i as usize] == 0 {
+                        game.board[level as usize][i as usize] = 0;
+                    }
+                }
+            }
+            let layer_width = get_layer_width(game.board, level);
+            if layer_width == 0 {
+                game_over = true
+            } else {
+                speed *= 0.97;
+                level += 1;
+                for i in 0..layer_width {
+                    game.board[level as usize][i as usize] = 1;
+                }
+            }
+        }
+
+        if game_over {
+            // TODO
+        } else if get_time() - last_update > speed {
+            last_update = get_time();
+            match move_direction {
+                Direction::LEFT => {
+                    let first = game.board[level as usize][0];
+                    if first == 1 {
+                        move_direction = Direction::RIGHT
+                    }
+                }
+                Direction::RIGHT => {
+                    let last = game.board[level as usize][BOARD_WIDTH - 1];
+                    if last == 1 {
+                        move_direction = Direction::LEFT
+                    }
+                }
+            }
+            match move_direction {
+                Direction::LEFT => {
+                    let mut i = 0;
+                    loop {
+                        if game.board[level as usize][i as usize] == 1 {
+                            game.board[level as usize][i as usize] = 0;
+                            game.board[level as usize][i - 1 as usize] = 1;
+                        }
+                        if i == BOARD_WIDTH - 1 {
+                            break;
+                        }
+                        i += 1;
+                    }
+                }
+                Direction::RIGHT => {
+                    let mut i = BOARD_WIDTH - 1;
+                    loop {
+                        if game.board[level as usize][i as usize] == 1 {
+                            game.board[level as usize][i as usize] = 0;
+                            game.board[level as usize][(i + 1) as usize] = 1;
+                        }
+                        if i == 0 {
+                            break;
+                        }
+                        i -= 1;
+                    }
+                }
+            }
+        }
+
+        for (j, row) in game.board.iter().rev().enumerate() {
+            for (i, el) in row.iter().enumerate() {
+                if *el == 1 {
+                    let x = i as f32 * 10.0 + 8.0;
+                    let y = j as f32 * 10.0 + 32.0;
+                    draw_rectangle(x, y, 8.0, 8.0, GREEN);
+                }
+            }
+        }
+
+        draw_text(format!("LEVEL: {}", level).as_str(), 8.0, 20.0, 20.0, GREEN);
+
+        next_frame().await
+    }
+}
