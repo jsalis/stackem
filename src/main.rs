e016cee5042803a22da25b9dcbc45dcf1f941a46
John Salis <jsalis531@gmail.com>
2021-07-20 17:49:55 -0400

    refactor: use contiguous array for board data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use macroquad::prelude::*;
 
-const BOARD_WIDTH: usize = 32;
-const BOARD_HEIGHT: usize = 32;
+const BOARD_WIDTH: u16 = 32;
+const BOARD_HEIGHT: u16 = 32;
+const BOARD_SIZE: usize = BOARD_WIDTH as usize * BOARD_HEIGHT as usize;
 const CELL_SIZE: f32 = 12.0;
-const INITIAL_LAYER_WIDTH: u8 = 12;
+const INITIAL_LAYER_WIDTH: u16 = 12;
 
 enum Direction {
     LEFT,
@@ -12,25 +13,33 @@ enum Direction {
 
 #[derive(Debug)]
 struct Game {
-    board: [[u8; BOARD_WIDTH]; BOARD_HEIGHT],
+    board: [u16; BOARD_SIZE],
     is_over: bool,
 }
 
 impl Game {
     fn new() -> Game {
         Game {
-            board: [[0; BOARD_WIDTH]; BOARD_HEIGHT],
+            board: [0; BOARD_SIZE],
             is_over: false,
         }
     }
-}
 
-fn get_layer_width(board: [[u8; BOARD_WIDTH]; BOARD_HEIGHT], level: u8) -> u8 {
-    let mut count: u8 = 0;
-    for el in board[level as usize].iter() {
-        count += el;
+    fn set_cell(&mut self, x: u16, y: u16, val: u16) {
+        self.board[(x + (y * BOARD_WIDTH)) as usize] = val;
     }
-    count
+
+    fn get_cell(&self, x: u16, y: u16) -> u16 {
+        self.board[(x + (y * BOARD_WIDTH)) as usize]
+    }
+
+    fn get_layer_width(&self, level: u16) -> u16 {
+        let mut count = 0;
+        for x in 0..BOARD_WIDTH {
+            count += self.get_cell(x, level);
+        }
+        count
+    }
 }
 
 #[macroquad::main("Stackem")]
@@ -40,8 +49,9 @@ async fn main() {
     let mut last_update = get_time();
     let mut speed = 0.1;
     let mut level = 0;
+
     for i in 0..INITIAL_LAYER_WIDTH {
-        game.board[level as usize][i as usize] = 1;
+        game.set_cell(i, level, 1);
     }
 
     loop {
@@ -51,31 +61,36 @@ async fn main() {
             game.is_over = false;
             speed = 0.1;
             level = 0;
-            for i in 0..BOARD_HEIGHT {
-                for j in 0..BOARD_WIDTH {
-                    game.board[i][j] = 0;
+
+            for y in 0..BOARD_HEIGHT {
+                for x in 0..BOARD_WIDTH {
+                    game.set_cell(x, y, 0);
                 }
             }
+
             for i in 0..INITIAL_LAYER_WIDTH {
-                game.board[level as usize][i as usize] = 1;
+                game.set_cell(i, level, 1);
             }
         }
 
         if !game.is_over && is_key_pressed(KeyCode::Down) {
             if level > 0 {
                 for i in 0..BOARD_WIDTH {
-                    if game.board[(level - 1) as usize][i as usize] == 0 {
-                        game.board[level as usize][i as usize] = 0;
+                    if game.get_cell(i, level - 1) == 0 {
+                        game.set_cell(i, level, 0);
                     }
                 }
             }
-            let layer_width = get_layer_width(game.board, level);
+
+            let layer_width = game.get_layer_width(level);
+
             if layer_width > 0 {
                 speed *= 0.97;
                 level += 1;
-                if (level as usize) < BOARD_HEIGHT {
+
+                if level < BOARD_HEIGHT {
                     for i in 0..layer_width {
-                        game.board[level as usize][i as usize] = 1;
+                        game.set_cell(i, level, 1);
                     }
                 } else {
                     game.is_over = true;
@@ -85,35 +100,29 @@ async fn main() {
             }
         }
 
-        if game.is_over {
-            if (level as usize) == BOARD_HEIGHT {
-                draw_text(format!("YOU ARE WINNER!").as_str(), 8.0, 60.0, 60.0, GREEN);
-            } else {
-                draw_text(format!("GAME OVER").as_str(), 8.0, 60.0, 60.0, GREEN);
-            }
-        } else if get_time() - last_update > speed {
+        if !game.is_over && get_time() - last_update > speed {
             last_update = get_time();
+
             match move_direction {
                 Direction::LEFT => {
-                    let first = game.board[level as usize][0];
-                    if first == 1 {
+                    if game.get_cell(0, level) == 1 {
                         move_direction = Direction::RIGHT
                     }
                 }
                 Direction::RIGHT => {
-                    let last = game.board[level as usize][BOARD_WIDTH - 1];
-                    if last == 1 {
+                    if game.get_cell(BOARD_WIDTH - 1, level) == 1 {
                         move_direction = Direction::LEFT
                     }
                 }
             }
+
             match move_direction {
                 Direction::LEFT => {
                     let mut i = 0;
                     loop {
-                        if game.board[level as usize][i as usize] == 1 {
-                            game.board[level as usize][i as usize] = 0;
-                            game.board[level as usize][i - 1 as usize] = 1;
+                        if game.get_cell(i, level) == 1 {
+                            game.set_cell(i, level, 0);
+                            game.set_cell(i - 1, level, 1);
                         }
                         if i == BOARD_WIDTH - 1 {
                             break;
@@ -124,9 +133,9 @@ async fn main() {
                 Direction::RIGHT => {
                     let mut i = BOARD_WIDTH - 1;
                     loop {
-                        if game.board[level as usize][i as usize] == 1 {
-                            game.board[level as usize][i as usize] = 0;
-                            game.board[level as usize][(i + 1) as usize] = 1;
+                        if game.get_cell(i, level) == 1 {
+                            game.set_cell(i, level, 0);
+                            game.set_cell(i + 1, level, 1);
                         }
                         if i == 0 {
                             break;
@@ -137,18 +146,25 @@ async fn main() {
             }
         }
 
-        for (j, row) in game.board.iter().rev().enumerate() {
-            for (i, el) in row.iter().enumerate() {
-                if *el == 1 {
-                    let x = i as f32 * (CELL_SIZE + 2.0) + 8.0;
-                    let y = j as f32 * (CELL_SIZE + 2.0) + 80.0;
-                    draw_rectangle(x, y, CELL_SIZE, CELL_SIZE, GREEN);
+        if game.is_over {
+            if level == BOARD_HEIGHT {
+                draw_text(format!("YOU ARE WINNER!").as_str(), 8.0, 60.0, 60.0, GREEN);
+            } else {
+                draw_text(format!("GAME OVER").as_str(), 8.0, 60.0, 60.0, GREEN);
+            }
+        }
+
+        for y in 0..BOARD_HEIGHT {
+            for x in 0..BOARD_WIDTH {
+                if game.get_cell(x, y) == 1 {
+                    let rx = x as f32 * (CELL_SIZE + 2.0) + 8.0;
+                    let ry = (BOARD_HEIGHT as f32 - y as f32) * (CELL_SIZE + 2.0) + 80.0;
+                    draw_rectangle(rx, ry, CELL_SIZE, CELL_SIZE, GREEN);
                 }
             }
         }
 
         draw_text(format!("LEVEL: {}", level).as_str(), 8.0, 20.0, 20.0, GREEN);
-
         next_frame().await
     }
 }
